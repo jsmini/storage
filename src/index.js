95cf7ccf92056901263062b72e9be31fb461462d
zenquan <1847867150@qq.com>
2019-06-02 15:34:43 +0800

    init: init project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
-import { yan } from './test.js';
+class Storage {
+  constructor () {
+      this.ls = window.localStorage;
+      this.ss = window.sessionStorage;
+  }
 
-import yan2 from './test.js';
+  /*-----------------localStorage---------------------*/
+  /*设置localStorage*/
+  setLocal(key, val) {
+      var setting = arguments[0];
+      if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object'){
+          for(var i in setting){
+              this.ls.setItem(i, JSON.stringify(setting[i]))
+          }
+      }else{
+          this.ls.setItem(key, JSON.stringify(val))
+      }
+      
+  }
 
-console.log(yan);
-console.log(yan2);
+  /*获取localStorage*/
+  getLocal(key) {
+      if (key) return JSON.parse(this.ls.getItem(key))
+      return null;
+      
+  }
 
-var a = 1 + 1;
-var b = a;
-console.log(a);
-console.log(b);
+  /*移除localStorage*/
+  removeLocal(key) {
+      this.ls.removeItem(key)
+  }
 
-export const name = 'base';
+  /*移除所有localStorage*/
+  clearLocal() {
+      this.ls.clear()
+  }
 
+
+  /*-----------------sessionStorage---------------------*/
+  /*设置sessionStorage*/
+  setSession(key, val) {
+      var setting = arguments[0];
+      if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object'){
+          for(var i in setting){
+              this.ss.setItem(i, JSON.stringify(setting[i]))
+          }
+      }else{
+          this.ss.setItem(key, JSON.stringify(val))
+      }
+      
+  }
+
+  /*获取sessionStorage*/
+  getSession(key) {
+      if (key) return JSON.parse(this.ss.getItem(key))
+      return null;
+      
+  }
+
+  /*移除sessionStorage*/
+  removeSession(key) {
+      this.ss.removeItem(key)
+  }
+
+  /*移除所有sessionStorage*/
+  clearSession() {
+      this.ss.clear()
+  }
+  
+}
+
+export default Storage;
